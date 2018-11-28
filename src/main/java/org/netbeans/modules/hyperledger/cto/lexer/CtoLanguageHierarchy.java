73e4e9cd45d2cf9d8ff90b29dd13c58133058e26
Mario-S <spindizzy@outlook.com>
2018-11-28 22:31:35 +0100

    no enum

--- src/main/java/org/netbeans/modules/hyperledger/cto/lexer/CtoLanguageHierarchy.java
+++ src/main/java/org/netbeans/modules/hyperledger/cto/lexer/CtoLanguageHierarchy.java
@@ -1,11 +1,14 @@
 package org.netbeans.modules.hyperledger.cto.lexer;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import org.antlr.v4.runtime.Vocabulary;
 import org.netbeans.modules.hyperledger.cto.FileType;
+import org.netbeans.modules.hyperledger.cto.grammar.CtoLexer;
 import org.netbeans.spi.lexer.LanguageHierarchy;
 import org.netbeans.spi.lexer.Lexer;
 import org.netbeans.spi.lexer.LexerRestartInfo;
@@ -13,19 +16,45 @@ import org.netbeans.spi.lexer.LexerRestartInfo;
 /**
  *
  */
-public class CtoLanguageHierarchy extends LanguageHierarchy<CtoTokenId>{
-    
+public class CtoLanguageHierarchy extends LanguageHierarchy<CtoTokenId> {
+
+    enum Category {
+        keyword, primitive, field, operator, separator, value, comment
+    }
+
     private final List<CtoTokenId> tokens;
     private final Map<Integer, CtoTokenId> idToToken;
-    
-    public CtoLanguageHierarchy() {
-        
-        idToToken = new HashMap<>();
-        
-        tokens = Arrays.asList(CtoTokenId.values());
-        tokens.forEach(token -> idToToken.put(token.ordinal(), token));
-    } 
 
+    public CtoLanguageHierarchy() {
+
+        Vocabulary vocabulary = CtoLexer.VOCABULARY;
+
+        idToToken = new HashMap<>();
+        tokens = new ArrayList<>();
+        int max = vocabulary.getMaxTokenType();
+        for (int i = 1; i < max; i++) {
+            CtoTokenId token = new CtoTokenId(vocabulary.getDisplayName(i), getCategory(i), i);
+            tokens.add(token);
+        }
+        tokens.forEach(token -> idToToken.put(token.ordinal(), token));
+    }
+
+    private String getCategory(int token) {
+        if (token < CtoLexer.BOOLEAN) {
+            return Category.keyword.name();
+        } else if (token < CtoLexer.DECIMAL_LITERAL) {
+            return Category.primitive.name();
+        } else if (token < CtoLexer.LPAREN) {
+            return Category.value.name();
+        } else if (token < CtoLexer.ASSIGN) {
+            return Category.separator.name();
+        } else if (token < CtoLexer.ELLIPSIS) {
+            return Category.operator.name();
+        } else if (token < CtoLexer.WS) {
+            return Category.field.name();
+        }
+        return Category.comment.name();
+    }
 
     @Override
     protected Collection<CtoTokenId> createTokenIds() {
@@ -41,7 +70,7 @@ public class CtoLanguageHierarchy extends LanguageHierarchy<CtoTokenId>{
     protected String mimeType() {
         return FileType.MIME;
     }
-    
+
     CtoTokenId getToken(int id) {
         return idToToken.get(id);
     }
--- src/main/java/org/netbeans/modules/hyperledger/cto/lexer/CtoTokenId.java
+++ src/main/java/org/netbeans/modules/hyperledger/cto/lexer/CtoTokenId.java
@@ -5,61 +5,26 @@ import org.netbeans.api.lexer.TokenId;
 /**
  *
  */
-public enum CtoTokenId implements TokenId {
+public class CtoTokenId implements TokenId{
     
-    ABSTRACT("abstract", "keyword"),
-    ASSET("asset", "keyword"),
-    CLASS("class", "keyword"),
-    CONCEPT("concept", "keyword"),
-    DEFAULT("default", "keyword"),
-    ENUM("enum", "keyword"),
-    EVENT("event", "keyword"),
-    EXTENDS("extends", "keyword"),
-    IDENTIFIED("identified by", "keyword"),
-    IMPORT("import", "keyword"),
-    NAMESPACE("namespace", "keyword"),
-    OPTIONAL("optional", "keyword"),
-    PARTICIPANT("participant", "keyword"),
-    RANGE("range", "keyword"),
-    REGEX("regex", "keyword"),
-    TRANSACTION("transaction", "keyword"),
-    
-    BOOLEAN("Boolean", "primitive"),
-    DATE_TIME("DateTime", "primitive"),
-    DOUBLE("Double", "primitive"),
-    INTEGER("Integer", "primitive"),
-    LONG("Long", "primitive"),
-    STRING("String", "primitive"),
-    
-    LPAREN("(", "separator"),
-    RPAREN(")", "separator"),
-    LBRACE("{", "separator"),
-    RBRACE("}", "separator"),
-    LBRACKET("[", "separator"),
-    RBRACKET("]", "separator"),
-    
-    SEMI(";", "operator"),
-    COMMA(",", "operator"),
-    DOT(".", "operator"),
-    COLON(":", "operator"),
-    ASSIGN("=", "operator"),
-    STAR("*", "operator"),
-    AT("@", "operator"),
-    TRIPLE_DOT("...", "operator"),
-    
-    REF("-->", "field"),
-    FIELD("o", "field");
-
-    private final String fixedText;
+    private final String name;
     private final String primaryCategory;
-
-    CtoTokenId(String fixedText, String primaryCategory) {
-        this.fixedText = fixedText;
+    private final int id;
+    
+    public CtoTokenId(String name, String primaryCategory, int id) {
+        this.name = name;
         this.primaryCategory = primaryCategory;
+        this.id = id;
     }
 
-    public String fixedText() {
-        return fixedText;
+    @Override
+    public String name() {
+        return name;
+    }
+
+    @Override
+    public int ordinal() {
+        return id;
     }
 
     @Override
@@ -69,7 +34,7 @@ public enum CtoTokenId implements TokenId {
 
     @Override
     public String toString() {
-        return "CtoTokenId{" + "text=" + fixedText + ", primaryCategory=" + primaryCategory + '}';
+        return "CtoTokenId{" + "name=" + name + ", primaryCategory=" + primaryCategory + ", id=" + id + '}';
     }
 
 }
