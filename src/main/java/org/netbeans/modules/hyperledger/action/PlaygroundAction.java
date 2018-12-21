876781c8b804f40eeb543432dce97517d401ab70
Mario-S <spindizzy@outlook.com>
2018-12-21 18:44:14 +0100

    action to start playground

---
+++ src/main/java/org/netbeans/modules/hyperledger/action/PlaygroundAction.java
@@ -0,0 +1,43 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.netbeans.modules.hyperledger.action;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import org.openide.awt.ActionID;
+import org.openide.awt.ActionReference;
+import org.openide.awt.ActionRegistration;
+import org.openide.util.NbBundle.Messages;
+
+@ActionID(
+        category = "Tools",
+        id = "org.netbeans.modules.hyperledger.action.PlaygroundAction"
+)
+@ActionRegistration(
+        displayName = "#CTL_PlaygroundAction"
+)
+@ActionReference(path = "Menu/Tools", position = 350)
+@Messages("CTL_PlaygroundAction=Hyperleder Playground")
+public final class PlaygroundAction implements ActionListener {
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO implement action body
+    }
+}
