b63b788e162ef4b4ed786dd7acd459c3858ada70
Mario-S <spindizzy@outlook.com>
2019-02-06 19:37:00 +0100

    class for resources

--- src/main/java/org/netbeans/modules/hyperledger/cto/grammar/ParserListener.java
+++ src/main/java/org/netbeans/modules/hyperledger/cto/grammar/ParserListener.java
@@ -18,12 +18,16 @@
  */
 package org.netbeans.modules.hyperledger.cto.grammar;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.TreeSet;
 import java.util.stream.Collectors;
+import org.antlr.v4.runtime.ParserRuleContext;
 import org.antlr.v4.runtime.tree.ErrorNode;
 import org.antlr.v4.runtime.tree.TerminalNode;
+import org.netbeans.modules.hyperledger.cto.CtoResource;
 
 /**
  *
@@ -31,16 +35,39 @@ import org.antlr.v4.runtime.tree.TerminalNode;
  */
 public final class ParserListener extends CtoParserBaseListener {
 
+    @Deprecated
     private final Map<String, Integer> members = new TreeMap<>();
+    
+    private final Collection<CtoResource> resources = new TreeSet<>();
 
+    @Deprecated
     public Map<String, Integer> getMembers() {
         return members;
     }
 
+    public Collection<CtoResource> getResources() {
+        return resources;
+    }
+
+    @Deprecated
     private void addNode(TerminalNode node, int id) {
         if (node != null && !(node instanceof ErrorNode)) {
             members.put(node.getText(), id);
         }
+    }
+    
+    private void addNode(TerminalNode node, int type, int line) {
+        if (node != null && !(node instanceof ErrorNode)) {
+            addNode(node.getText(), type, line);
+        }
+    }
+    
+    private void addNode(String text, int type, int line) {
+        resources.add(new CtoResource(text, type, line));
+    }
+    
+    private int getLine(ParserRuleContext ctx) {
+        return ctx.getStart().getLine();
     }
 
     @Override
@@ -50,36 +77,43 @@ public final class ParserListener extends CtoParserBaseListener {
             List<TerminalNode> identifiers = qualCtx.IDENTIFIER();
             String name = identifiers.stream().map(TerminalNode::getText).collect(Collectors.joining("."));
             members.put(name, CtoLexer.NAMESPACE);
+            addNode(name, CtoLexer.NAMESPACE, getLine(ctx));
         }
     }
 
     @Override
     public void exitAssetDeclaration(CtoParser.AssetDeclarationContext ctx) {
         addNode(ctx.IDENTIFIER(), CtoLexer.ASSET);
+        addNode(ctx.IDENTIFIER(), CtoLexer.ASSET, getLine(ctx));
     }
 
     @Override
     public void exitParticipantDeclaration(CtoParser.ParticipantDeclarationContext ctx) {
         addNode(ctx.IDENTIFIER(), CtoLexer.PARTICIPANT);
+        addNode(ctx.IDENTIFIER(), CtoLexer.PARTICIPANT, getLine(ctx));
     }
 
     @Override
     public void exitTransactionDeclaration(CtoParser.TransactionDeclarationContext ctx) {
         addNode(ctx.IDENTIFIER(), CtoLexer.TRANSACTION);
+        addNode(ctx.IDENTIFIER(), CtoLexer.TRANSACTION, getLine(ctx));
     }
 
     @Override
     public void exitEventDeclaration(CtoParser.EventDeclarationContext ctx) {
         addNode(ctx.IDENTIFIER(), CtoLexer.EVENT);
+        addNode(ctx.IDENTIFIER(), CtoLexer.EVENT, getLine(ctx));
     }
 
     @Override
     public void exitEnumDeclaration(CtoParser.EnumDeclarationContext ctx) {
         addNode(ctx.IDENTIFIER(), CtoLexer.ENUM);
+        addNode(ctx.IDENTIFIER(), CtoLexer.ENUM, getLine(ctx));
     }
 
     @Override
     public void exitConceptDeclaration(CtoParser.ConceptDeclarationContext ctx) {
         addNode(ctx.IDENTIFIER(0), CtoLexer.CONCEPT);
+        addNode(ctx.IDENTIFIER(0), CtoLexer.CONCEPT, getLine(ctx));
     }
 }
